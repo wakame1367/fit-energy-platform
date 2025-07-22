21887db6b00400dd4e02540c5ce783564c6a057c
Kota Yuhara <hotwater1367@gmail.com>
2025-07-22 22:20:24 +0900

    feat: add IAM role and policies for S3 access in lambroll

--- infra/terrform/main.tf
+++ infra/terrform/main.tf
@@ -5,6 +5,46 @@ resource "aws_ecr_repository" "app" {
   }
 }
 
+resource "aws_s3_bucket" "infra" {
+  bucket = local.project
+}
+
+resource "aws_iam_role" "lambroll_lambda_role" {
+  name = "${local.project}-lambroll-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "basic_exec" {
+  role       = aws_iam_role.lambroll_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "s3_access" {
+  name = "${local.project}-s3-access"
+  role = aws_iam_role.lambroll_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["s3:PutObject", "s3:GetObject"],
+      Resource = "${aws_s3_bucket.infra.arn}/*"
+    }]
+  })
+}
+
+output "lambroll_lambda_role_arn" {
+  value = aws_iam_role.lambroll_lambda_role.arn
+}
+
 output "ecr_url" {
   value = aws_ecr_repository.app.repository_url
 }
